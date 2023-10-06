ce24c9a2e6008424c6945ab84ab94270c4bf1b1f
Huakun Shen <huakun.shen@huakunshen.com>
2023-10-06 18:57:55 -0400

    Add link in header

--- apps/web/src/app/components/header.tsx
+++ apps/web/src/app/components/header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { GitHubIcon } from "ui";
 
 interface PropType {
@@ -7,10 +8,12 @@ interface PropType {
 export default function Footer(props: PropType) {
   return (
     <div className={`${props.className} px-10 pt-5`}>
-      <span className="flex items-center space-x-2">
-        <GitHubIcon className="w-10 h-10" />
-        <span className="text-xl font-semibold font-mono">Skyline</span>
-      </span>
+      <div className="flex">
+        <Link href="/" className="flex items-center space-x-2">
+          <GitHubIcon className="w-10 h-10" />
+          <span className="text-xl font-semibold font-mono">Skyline</span>
+        </Link>
+      </div>
     </div>
   );
 }
