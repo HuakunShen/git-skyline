676b0394e70bd99d27db123c752d7806c8708e3e
Huakun Shen <huakun.shen@huakunshen.com>
2024-07-17 03:11:37 -0400

    💚 Fix CI. Add missing @apollo/client dep.

--- scripts/init-env.ts
+++ scripts/init-env.ts
@@ -1,10 +1,13 @@
 import { writeFileSync } from "fs";
 import { resolve } from "path";
 
-const envContent = `
+if (process.env.GITHUB_API_TOKEN) {
+  const envContent = `
 GITHUB_API_TOKEN=${process.env.GITHUB_API_TOKEN}
 `;
-writeFileSync(
-  resolve(__dirname, "../packages/github-graphql/.env"),
-  envContent
-);
+
+  writeFileSync(
+    resolve(__dirname, "../packages/github-graphql/.env"),
+    envContent
+  );
+}
