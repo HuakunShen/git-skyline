4df507c2afc574333a430118290e7e750c751aa1
Huakun Shen <huakun.shen@huakunshen.com>
2024-07-17 02:17:58 -0400

    ⬆️ Upgrade turbo to 2.0.7. Replace bun with pnpm as main package manager.

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["ui", "three"],
+  transpilePackages: ["ui", "three", "@git-skyline/github-graphql"],
 };
 
 module.exports = nextConfig;
