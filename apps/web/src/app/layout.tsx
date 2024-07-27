f92ff3439a1bd00b335551b11802bd31c6af2530
Huakun Shen <huakun.shen@huakunshen.com>
2024-07-26 20:29:21 -0400

    Add vercel SpeedInsights

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,9 +17,8 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
-      <body className={`${inter.className} h-screen`}>
-        {children}
-      </body>
+      <SpeedInsights />
+      <body className={`${inter.className} h-screen`}>{children}</body>
     </html>
   );
 }
