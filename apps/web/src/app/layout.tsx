b4622ebcdb8c22ab599ba696b277cb666a945d70
Huakun Shen <huakun.shen@huakunshen.com>
2024-07-26 20:36:55 -0400

    Add google tag

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <Analytics />
       <SpeedInsights />
+      <GoogleAnalytics gaId="G-K3T0E06JV4" />
       <body className={`${inter.className} h-screen`}>{children}</body>
     </html>
   );
